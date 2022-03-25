9d0f3e654f704bc4488347c4b2fbc034cad7ae77
Michael Stokes <michael.stokes.347@gmail.com>
2022-03-25 16:51:36 -0500

    code practice

---
+++ main.js
@@ -0,0 +1,13 @@
+
+     // TODO: practice
+function switchOfStuff(val) {
+    let answer = "";
+    // Only change code below this line
+
+
+
+    // Only change code above this line
+    return answer;
+}
+
+switchOfStuff(1);
